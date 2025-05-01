18f0f96d67ed0932d86b7b3bea3e606f566b3d8c
Paweł Staniul <pstaniul@gmail.com>
2025-05-01 12:37:34 +0200

    Dodano komponent ClassAddComponent

---
+++ frontend/src/app/features/admin/class-add/class-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-add',
+  imports: [],
+  templateUrl: './class-add.component.html',
+  styleUrl: './class-add.component.css'
+})
+export class ClassAddComponent {
+
+}
