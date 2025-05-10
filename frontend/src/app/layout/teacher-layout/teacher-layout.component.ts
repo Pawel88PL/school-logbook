c5e55b00fa14ea8e0879eddec205b0c3f4fcacfa
Paweł Staniul <pstaniul@gmail.com>
2025-05-10 12:08:23 +0200

    Utworzono podstawowy layout dla nauczyciela

--- frontend/src/app/layout/teacher-layout/teacher-layout.component.ts
+++ frontend/src/app/layout/teacher-layout/teacher-layout.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { AdminService } from '../../core/services/admin.service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-teacher-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    RouterModule,
+  ],
   templateUrl: './teacher-layout.component.html',
   styleUrl: './teacher-layout.component.css'
 })
-export class TeacherLayoutComponent {
+export class TeacherLayoutComponent implements OnInit, OnDestroy {
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  isSmallScreen: boolean = false;
+  sidenavOpened: boolean = false;
+  sessionIndex: string | null = null;
+
+  subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private adminService: AdminService,
+    private breakpointObserver: BreakpointObserver,
+  ) { }
+
+  ngOnInit(): void {
+    this.adjustSidenav();
+    this.initializeSubscription();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  adjustSidenav(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 1200px)'])
+      .subscribe((result: BreakpointState) => {
+        this.isSmallScreen = result.matches;
+        this.sidenavOpened = !this.isSmallScreen;
+      });
+  }
+
+  initializeSubscription(): void {
+    this.subscriptions.add(
+      this.adminService.toggle$.subscribe(() => {
+        this.sidenav.toggle();
+      })
+    );
+  }
 }
